186fdc8ae7b52241d00bed8749ba0e570ce3ee49
Ananthakrishnan21 <120053092+Ananthakrishnan21@users.noreply.github.com>
2023-07-14 11:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // your code here
 let heading=document.getElementById('url')
 let button=document.getElementById('button')
+let name=document.getElementById('name')
+let year=document.getElementById('year')
 button.addEventListener('click',update)
 function update(event){
 	event.preventDefault();
-	heading.innerHTML="Ananthan"
+	if(name.value=='' && year.value!=''){
+		heading.innerHTML=`https://localhost:8080/?${year.name}=${year.value}`
+	}
+	else if(name.value!='' && year.value==''){
+		heading.innerHTML=`https://localhost:8080/?${name.name}=${name.value}`
+	}
+	else if(name.value!='' && year.value!=''){
+		heading.innerHTML=`https://localhost:8080/?${name.name}=${name.value}&${year.name}=${year.value}`
+	}
+	else{
+		heading.innerHTML=`https://localhost:8080/`}
 }

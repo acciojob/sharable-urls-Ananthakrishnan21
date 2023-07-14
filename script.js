c761104c233562bdbec9f5ac4632a105be59e814
Ananthakrishnan21 <120053092+Ananthakrishnan21@users.noreply.github.com>
2023-07-14 11:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+let heading=document.getElementById('url')
+let button=document.getElementById('button')
+button.addEventListener('click',update)
+function update(event){
+	event.preventDefault();
+	heading.innerHTML="Ananthan"
+}
